3bcead71caca3d3a33ce1b43d5674ffdbac74c3b
2114303 <146530058+2114303@users.noreply.github.com>
2024-10-05 03:27:56 +0530

    StringRunner.java

--- StringRunner.java
+++ StringRunner.java
@@ -4,21 +4,58 @@ import java.io.*;
 import com.zoho.task.*;
 public class StringRunner{
 public static void main(String[] args){
-	Console input= System.console();
-	String str = input.readLine("Enter a string : ");
+	System.out.println("=========== String Tasks =============");
+	System.out.print("1.Length of the String\n2.Convert String into char array\n3.Find penultimate character of a string\n4.No. of occurence of character\n5.Greatest position of a character\n6.Displaying last Few characters of a string\n7.Displaying first few characters of a string\n8.Replacing characters in a string\n9.check whether the string starts with the original string \n10.check whether the string ends with the original string\n11.UPPERCASE\n12.lowercase\n13.Reverse a string\n14.Multiple words in a single line\n15.Convert a string into string array using a seperator\n16.Comparing strings\n17.Removing extra spaces in the beginning and the end\n18.Merging strings with a character in between\n19.Exit\n");
+boolean exit;
+do{
+	Scanner scanner = new Scanner(System.in);
+	int selectOperation;
+	String str="";
+	boolean invalidIndex,emptyString;
+	exit = true;
+	do{	System.out.println();
+		selectOperation=getValidIntegerInput(scanner,"Select an operation : ");
+		if(0<selectOperation&&selectOperation<20){
+			break;
+			}
+		System.out.println("Please select a valid number(1-18)");
+	}while(true);
+	scanner.nextLine();
+	switch(selectOperation){
+	case 1:{
+	str = getValidInputString(scanner,"Enter a String : ");
 	StringTask word = new StringTask(str);
 	System.out.println("Length of the entered String : " + word.getLength());
+	break;
+	}
+	case 2:{
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
 	System.out.println("Converting String into char Array : "+ Arrays.toString(word.convertToArr()));
+	break;
+	}
+	case 3:{
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
 	if(word.penultimateChar()=='\0'){
 		System.out.println("Penultimate character does not exist");
 		}
 	else{
 		System.out.println("Penultimate character of the entered string : " + word.penultimateChar());
 	}
-	Scanner scanner = new Scanner(System.in);
+	break;
+	}
+	case 4:{
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
 	System.out.print("Enter a char to check its no. of occurrences : ");
 	char character1 = scanner.next().charAt(0);
 	System.out.println("No of occurence of the entered character :"+ word.noOfOccurences(character1));
+	break;
+	}
+	case 5:{
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
 	System.out.print("Enter a char to find its greatest position : ");
 	char character2 = scanner.next().charAt(0);
 	if(word.greatestPosition(character2)==-1){
@@ -27,47 +64,122 @@ public static void main(String[] args){
 	else{
 		System.out.println("Greatest position of the entered character :"+ word.greatestPosition(character2));
 	}
-	System.out.println("Last five characters of the given string : "+ word.lastFiveChar());
-	System.out.println("First three characters of the given string :"+ word.firstThreeChar());
-	System.out.print("Enter a string to replace first three chatracters :");
-	String replace = scanner.next();
-	if(word.replace(replace)==null){
-		System.out.println("The string is shorter than three characters, so it is not possible to replace the first three letters.");
-		}
-	else{
-	System.out.println("Modified String :" + word.replace(replace));
+	break;
 	}
-	System.out.print("Enter the String to check whether it ends with :");
-	String endString = scanner.next();
-	System.out.println(word.endWith(endString));
-	System.out.print("Enter the String to check whether it starts with :");
-	String startString = scanner.next();
-	System.out.println(word.startWith(startString));
+	case 6:{
+	System.out.println("---------- Displaying last few characters ----------");
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
+	do{	
+		int noOfLastFewChar = getValidIntegerInput(scanner,"Enter the No. of characters to be displayed :");
+		String lastFewChar = word.lastFewCharacters(noOfLastFewChar);
+		if(lastFewChar.equals("")){
+			System.out.println("Invalid index number");
+			}
+		else if(lastFewChar.length()>noOfLastFewChar){
+			System.out.println("Number exceeds the length of the string. Please try again.");
+		}
+		else{
+			System.out.println("Last " + noOfLastFewChar+" characters of the given string : "+ lastFewChar);
+			break;
+		}
+	}while(true);
+	break;
+	}
+	case 7:{
+	System.out.println("---------- Displaying first few characters ----------");
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
+	do{
+		int noOfFirstFewChar= getValidIntegerInput(scanner,"Enter the No. of characters to be displayed :");
+		String firstFewChar = word.firstFewCharacters(noOfFirstFewChar);
+		if(firstFewChar.equals("")){
+			System.out.println("Invalid index number");
+		}else if(firstFewChar.length()>noOfFirstFewChar){
+			System.out.println("Number exceeds the length of the string. Please try again.");
+		}else{
+			System.out.println("First "+noOfFirstFewChar+" characters of the given string :"+ firstFewChar);
+			break;
+		}
+	}while(true);
+	break;
+	}
+	case 8:{
+	System.out.println("---------- Replacing Strings !! ----------");
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);	
+	String replace = getValidInputString(scanner,"Enter the string to replace characters at the beginning :");
+	System.out.println("ModifiedString : "+ word.replacingChar(replace));
+	break;
+	}
+	case 9:{
+	System.out.println("===== Checking if the string starts with the original string =====");
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
+	String enteredString = getValidInputString(scanner,"Enter a String to check : ");
+	System.out.println("Entered string starts with the original string - "+ word.startWith(enteredString));
+	break;
+		}
+	case 10:{
+	System.out.println("===== Checking if the string ends with the original string =====");
+	str = getValidInputString(scanner,"Enter original String : ");
+	StringTask word = new StringTask(str);
+	String enteredString = getValidInputString(scanner,"Enter a String to check : ");
+	System.out.println("Entered string ends with the original string"+ word.endWith(enteredString));
+	break;
+	}
+	case 11:{
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
 	System.out.println("UPPERCASE : "+ word.upperCase());
+	break;
+	}
+	case 12:{
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
 	System.out.println("lowercase : "+ word.lowerCase());
+	break;
+	}
+	case 13:{
+	str = getValidInputString(scanner,"Enter a String : ");
+	StringTask word = new StringTask(str);
 	System.out.println("Reversed String : "+ word.reverse());
-	System.out.print("Enter multiple strings in a single line:");
-	scanner.nextLine();
-	String multiString = scanner.nextLine();
-	System.out.println("Multiple strings entered in a line : "+ multiString);
+	break;
+	}
+	case 14:{
+	String multiString;
+	multiString = getValidInputString(scanner,"Enter a string with spaces : ");
 	StringTask multiwords = new StringTask(multiString);
-	System.out.println("Multiple strings in a single line after removing spaces : "+multiwords.removeSpaces());
-	System.out.println("spaced strings to String arr : "+ Arrays.toString(multiwords.convertToStringArr()));
-	System.out.println();
+	System.out.println("strings with spaces in a single line after removing spaces : "+ multiwords.removeSpaces());
+	break;
+	}
+	case 15:{
+	System.out.println("===Converting string into string array using a seperator===");
+	String multiString;
+	multiString = getValidInputString(scanner,"Enter a String : ");
+	StringTask removeSpace = new StringTask(multiString);
+	String seperator = getValidInputString(scanner,"Enter seperator : ");
+	System.out.println("spaced strings to String arr : "+ Arrays.toString(removeSpace.convertToStringArr(seperator)));
+	break;
+	}
+	case 16:{
 	System.out.println("Comparing two strings whether it is equal or not");
-	System.out.print("Enter string1 :");
-	String mainString = scanner.next();
-	System.out.print("Enter string2 :");
-	String compareString = scanner.next();
+	String mainString,compareString;
+	mainString = getValidInputString(scanner,"Enter String 1 : ");
+	compareString = getValidInputString(scanner,"Enter String 2 : ");
 	System.out.println("String1 is equal to String2 (case sensitive) - "+ StringTask.compareIncludeCase(mainString,compareString));
 	System.out.println("String1 is equal to String2 (ignore case) - "+ StringTask.compareExcludeCase(mainString,compareString));
-	scanner.nextLine();
-	System.out.print("Enter a string which has spaces in the beginning and ending :");
-	String stringWithSpace = scanner.nextLine();
+	break;
+	}
+	case 17:{
+	System.out.println("====== Removing extra spaces in the beginning and end ======");
+	String stringWithSpace = getValidInputString(scanner,"Enter a string which has spaces in the beginning and ending :");
 	StringTask spacedString = new StringTask(stringWithSpace);
 	System.out.println("String after removing extra spaces :" + spacedString.noSpacesBeginEnd());
-	System.out.println();
-	System.out.println("Write mulitple strings to merge them in a single line seperated by (-)");
+	break;
+	}
+	case 18:{
+	System.out.println("===Write multiple strings to merge them in a single line seperated by a character===");
 	ArrayList<String> multipleStrings = new ArrayList<String>();
 	int n=1;
 	while(true){
@@ -75,9 +187,47 @@ public static void main(String[] args){
 	 	String toMerge= scanner.nextLine();
 	 	n++;
 	 	if (toMerge.isEmpty()){
-	 		break;}
+	 		break;
+	 		}
 	 	multipleStrings.add(toMerge);
 	 	}
-	System.out.println("Merged Strings with a - inbetween :"+ StringTask.mergingStrings(multipleStrings));
+	String mergingChar = getValidInputString(scanner,"Enter the character : ");
+	System.out.println("Merged Strings with a character inbetween : "+ StringTask.mergingStrings(multipleStrings,mergingChar));
+	break;
+	}
+	case 19:{
+		exit = false;
+		break;
 	}
 	}
+	}while(exit);
+}
+public static String getValidInputString(Scanner scanner,String prompt){
+	String str;
+	boolean emptyString;
+	do{	
+		System.out.print(prompt);
+		str = scanner.nextLine();
+		emptyString = str.isEmpty();
+		if(emptyString){
+			System.out.println("Input cannot be empty");
+			}
+	}while(emptyString);
+	return str;
+	}
+public static int getValidIntegerInput(Scanner scanner, String prompt) {
+    int input = 0;
+    boolean validInput = false;
+    while (!validInput) {
+        System.out.print(prompt);
+        try {
+            input = scanner.nextInt();  
+            validInput = true;          
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+            scanner.next(); 
+    }
+	}
+	return input;
+}
+}	
